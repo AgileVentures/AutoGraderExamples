d3689563adb1230ef73736648c9ed7dfd80b639e
Sam Joseph <tansaku@gmail.com>
2014-03-18 06:30:34 +0000

    giving the grader a few teeth

--- git-immersion/autograder/mvp_spec.rb
+++ git-immersion/autograder/mvp_spec.rb
@@ -5,4 +5,11 @@ describe "Github" do
   it "should find a gitimmersion repository for the user [5 points]" do
     (Octokit.repository? ENV['GITHUB_USERNAME']+'/gitimmersion').should be_true
   end
+
+  it 'should have at least 3 recent commits in name of user  [15 points]' do
+      repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
+      commits = Octokit.commits_since(repo_name,'2014-01-01')
+      commits.count.should be > 3
+      commits.each {|c| c.author.login.should eq ENV['GITHUB_USERNAME']}
+  end
 end
