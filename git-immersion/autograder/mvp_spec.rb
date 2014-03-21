12933a7b8e541746e88f3a0f79f2c221c582f829
Paul McCulloch <hrfeels@gmail.com>
2014-03-21 14:55:49 -0700

    add check for one merge commit

--- git-immersion/autograder/mvp_spec.rb
+++ git-immersion/autograder/mvp_spec.rb
@@ -12,4 +12,11 @@ describe "Github" do
       commits.count.should be > 3
       commits.each {|c| c.author.login.should eq ENV['GITHUB_USERNAME']}
   end
+
+  it 'should have at least merge commit [5 points]' do
+    repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
+    commits = Octokit.commits_since(repo_name,'2014-01-01')
+    collection = commits.collect{|c| c.commit.message.include? 'merge'}
+    collection.should include(true)
+  end
 end
