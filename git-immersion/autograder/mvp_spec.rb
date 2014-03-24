fe40cf52679f060f1919455a3790fc38d1dfdf2c
Jacob <jhasson@uncc.edu>
2014-03-24 14:53:51 -0400

    Update mvp_spec.rb

--- git-immersion/autograder/mvp_spec.rb
+++ git-immersion/autograder/mvp_spec.rb
@@ -6,7 +6,7 @@ describe "Github" do
     (Octokit.repository? ENV['GITHUB_USERNAME']+'/gitimmersion').should be_true
   end
 
-   it "should have at least 3 recent commits in name of user  [10 points]" do
+   it "should have at least 3 recent commits in name of #{ENV['GITHUB_USERNAME']+'/gitimmersion'}  [10 points]" do
       repo_name = ENV['GITHUB_USERNAME']+'/gitimmersion'
       commits = Octokit.commits_since(repo_name,'2014-01-01')
       commits.count.should be > 3
