c5704cf599325b7391dc27994df55f2cc0cd95cf
Paul McCulloch <hrfeels@gmail.com>
2014-03-26 13:54:48 -0700

    give points even in case of forked repo, different points allotment

--- git-immersion/autograder/mvp_spec.rb
+++ git-immersion/autograder/mvp_spec.rb
@@ -4,27 +4,29 @@ require 'rspec'
 
 describe "Github" do
 
-#START_DATE = '2014-01-01'
+  # START_DATE = '2014-01-01'
   START_DATE = Date.today << 12 # months
 
-# This ENV var comes from GithubRspecGrader
+  # This ENV var comes from GithubRspecGrader
   USER_REPO = ENV['GITHUB_USERNAME']+'/gitimmersion'
 
   before (:all) do
-    # TODO AgileVentures key. Do these expire??
     # Rate limit is 5000/hr
     # This ENV var comes from travis env:global:secure or set by admin
-    # PAULS_GIT_IMMERSION_TOKEN is configured to allow only read of public access data
+    # TODO Do these expire??
+    # GIT_IMMERSION_TOKEN is configured to allow only read of public access data
     @token = ENV['GIT_IMMERSION_TOKEN']
-
-
     @client = Octokit::Client.new(:access_token => @token)
     @commits =  @client.commits_since(USER_REPO, START_DATE)
-    @client.repository(USER_REPO).parent.should be_nil
   end
 
-  it "should find a repository on github for: #{USER_REPO} [5 points]" do
+
+  it "should find a repository on github for: #{USER_REPO} [20 points]" do
     (@client.repository? USER_REPO).should be_true
+  end
+
+  it "should be a freshly created repo, not a fork  [50 points]" do
+    @client.repository(USER_REPO).parent.should be_nil
   end
 
   #TODO point per commit is possible?
@@ -32,11 +34,11 @@ describe "Github" do
     @commits.count.should be > 3
   end
 
-  it "should have at least 6 commits since #{START_DATE} [15 points]" do
+  it "should have at least 6 commits since #{START_DATE} [10 points]" do
     @commits.count.should be > 6
   end
 
-  it "should have at least 9 commits since #{START_DATE} [15 points]" do
+  it "should have at least 9 commits since #{START_DATE} [10 points]" do
     @commits.count.should be > 9
   end
 
